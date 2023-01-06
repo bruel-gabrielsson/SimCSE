9a7b7d3993dc585853ddea697dcc8439de200dc3
Rickard Brüel Gabrielsson <rbg@dhcp-10-31-96-71.dyn.MIT.EDU>
2023-01-06 14:55:36 -0500

    M

--- simcse/models.py
+++ simcse/models.py
@@ -175,7 +175,7 @@ def cl_forward(cls,
             # _tensor -= pooler_output.detach()
             # _tensor[mask_this_transform] =  
             #pooler_output[mask_this_transform] += torch.nn.Dropout(p=0.5, inplace=False)(pooler_output[mask_this_transform]) - pooler_output[mask_this_transform]
-            pooler_output += (torch.nn.Dropout(p=0.5, inplace=False)(pooler_output) - pooler_output) * big_mask
+            pooler_output = pooler_output + (torch.nn.Dropout(p=0.5, inplace=False)(pooler_output) - pooler_output) * big_mask
             # if not cls.config.transform_trainable:
             #     pooler_output[mask_this_transform] = pooler_output[mask_this_transform].detach()
 
