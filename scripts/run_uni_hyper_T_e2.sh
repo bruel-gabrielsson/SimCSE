ddc7080247db8d5e564394b3c349feb662918d96
Rickard Brüel Gabrielsson <rbg@31-38-220.wireless.csail.mit.edu>
2023-01-13 15:42:35 -0500

    M

--- scripts/run_uni_hyper_T_e2.sh
+++ scripts/run_uni_hyper_T_e2.sh
@@ -16,7 +16,7 @@
 
 declare -a layers=(0 1 2 3 4 5 6 7 8 9 10 11 12 13)
 declare -a batch_sizes=(64 128 256 512) ####
-declare -a learning_rates=(1e-5 3e-5 5e-5)
+declare -a learning_rates=("1e-5" "3e-5" "5e-5")
 declare -a devices=(2 3 4) 
 for layer in "${layers[@]}"
 do 
