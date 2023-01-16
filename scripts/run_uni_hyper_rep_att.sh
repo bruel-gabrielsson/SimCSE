734b280c6e1ee0e469a79aa7b2d21a808a228282
Rickard Brüel Gabrielsson <rbg@dhcp-10-29-20-243.dyn.MIT.EDU>
2023-01-16 12:59:30 -0500

    M

--- scripts/run_uni_hyper_rep_att.sh
+++ scripts/run_uni_hyper_rep_att.sh
@@ -14,26 +14,25 @@
 
 #export CUDA_VISIBLE_DEVICES="5"
 
-declare -a layers=(0 1 2 3 4 5 6 7 8 9 10 11 12 13)
+declare -a layers=(0)
 declare -a batch_sizes=(64 128 256 512) ####
 declare -a learning_rates=(1e-5 3e-5 5e-5)
-declare -a devices=(4 5 6 7) 
+declare -a devices=(2 3 4) 
 for layer in "${layers[@]}"
 do 
-    for learning_rate in "${learning_rates[@]}"
+    for batch_size in "${batch_sizes[@]}"
     do 
         device_index=-1
         for _ in "${devices[@]}"
         do
             device_index=$((device_index + 1))
 
-            batch_size=$((batch_sizes[device_index]))
+            learning_rate=${learning_rates[$device_index]}
             device=$((devices[device_index]))
             
             output_dir="/mnt2/brg/simcse-data/HYPER/ATT/ATT_L${layer}_b${batch_size}_lr${learning_rate}"
             echo "device ${device} batch_size ${batch_size} output_dir ${output_dir}"
             CUDA_VISIBLE_DEVICES="${device}" python train.py \
-                --transform_layer $layer \
                 --attention_probs_dropout_prob 0.1 \
                 --hidden_dropout_prob 0.0 \
                 --model_name_or_path bert-base-uncased \
