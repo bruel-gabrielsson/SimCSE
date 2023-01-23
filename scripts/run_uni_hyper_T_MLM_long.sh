4c11d945a202bda0ec00b9a56f4da7eaa198fdeb
Rickard Brüel Gabrielsson <rbg@dhcp-10-29-20-243.dyn.mit.edu>
2023-01-23 16:35:56 -0500

    M

--- scripts/run_uni_hyper_T_MLM_long.sh
+++ scripts/run_uni_hyper_T_MLM_long.sh
@@ -16,9 +16,9 @@
 
 #declare -a layers=(0 1 2 3 4 5 6 7 8 9 10 11 12 13)
 
-declare -a batch_sizes=(512) ####
+declare -a batch_sizes=(256) ####
 declare -a learning_rates=(5e-5)
-declare -a layers=(7 8 9)
+declare -a layers=(7)
 declare -a devices=(0) 
 for learning_rate in "${learning_rates[@]}"
 do 
