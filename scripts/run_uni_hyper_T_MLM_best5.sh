78f5004093bd84d074abec0226f26529aef4e6a3
Rickard Brüel Gabrielsson <rbg@31-38-220.wireless.csail.mit.edu>
2023-01-24 15:48:56 -0500

    M

--- scripts/run_uni_hyper_T_MLM_best5.sh
+++ scripts/run_uni_hyper_T_MLM_best5.sh
@@ -17,7 +17,7 @@
 #declare -a layers=(0 1 2 3 4 5 6 7 8 9 10 11 12 13)
 # /mnt/brg/simcse-data/HYPER/T_MLM/TMLM_L10_b256_lr5e-5
 
-declare -a batch_sizes=(256) ####
+declare -a batch_sizes=(128) ####
 declare -a learning_rates=(5e-5)
 declare -a layers=(10)
 declare -a devices=(7) 
