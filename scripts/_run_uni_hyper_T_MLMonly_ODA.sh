ee41d381d0438885deff1b6386cbc1f60fb87cfa
bruel-gabrielsson <rgabriel@stanford.edu>
2023-10-31 14:26:34 +0000

    Fix

--- scripts/_run_uni_hyper_T_MLMonly_ODA.sh
+++ scripts/_run_uni_hyper_T_MLMonly_ODA.sh
@@ -18,7 +18,7 @@ declare -a layers=(0 1 2 3 4 5 6 7 8 9 10 11 12 13)
 #declare -a layers=(3) # 0 1 2 3 4 5 6)
 declare -a batch_sizes=(64 128 256 512) ####
 declare -a learning_rates=(5e-5 1e-5 3e-5) #  5e-5)
-declare -a devices=(0 1 2) 
+declare -a devices=(3 4 5) 
 for layer in "${layers[@]}"
 do 
     for batch_size in "${batch_sizes[@]}"
@@ -33,12 +33,13 @@ do
             
             #output_dir="/mnt2/brg/simcse-data/HYPER/REG_MLM/REGMLM_L${layer}_b${batch_size}_lr${learning_rate}"
             #output_dir="/skunk-pod-storage-brg-40mit-2eedu-pvc/DATA/simcse-data/HYPER/REG_MLMO_ODA/REGMLMO_L${layer}_b${batch_size}_lr${learning_rate}"
-            output_dir="/mnt2/brg/simcse-data/HYPER/REG_MLMO_ODA/REGMLMO_L${layer}_b${batch_size}_lr${learning_rate}"
+            output_dir="/mnt2/brg/simcse-data/HYPER/T_MLMO_ODA/TMLMO_L${layer}_b${batch_size}_lr${learning_rate}"
             echo "device ${device} batch_size ${batch_size} output_dir ${output_dir}"
             CUDA_VISIBLE_DEVICES="${device}" python train.py \
                 --transform_layer $layer \
                 --higher_transform_p 0.5 \
                 --higher_dropout_p 0.5 \
+                --transform_trainable \
                 --do_mlm \
                 --attention_probs_dropout_prob 0.1 \
                 --hidden_dropout_prob 0.1 \
