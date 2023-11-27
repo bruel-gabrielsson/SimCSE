da51eeec78e493c3b1e6e35b9fa4c4dcb748326d
bruel-gabrielsson <rgabriel@stanford.edu>
2023-11-27 14:23:06 +0000

    M

--- export_supervised.py
+++ export_supervised.py
@@ -26,32 +26,40 @@ def iterate_directories(base_dir):
     for root, dirs, _ in os.walk(base_dir):
         #print("root", root)
         if 'SUPER_REG_NOV26_S' in root:
-            print("root", root)
+            print("root", root) 
+            # /mnt/brg/simcse-data/HYPER/SUPER_REG_NOV26_S1/SUPREG_L12_dr0.25_b64_lr0.0001_s1
 
-            #seed = extract_seed(root)
+            seed = extract_seed(root)
+            if seed is not None:
 
-            this_seed_data = {}
+                #this_seed_data = {}
 
-            # This will iterate over all the directories in the root
-            for dir in dirs:
-                
-                num = extract_number(dir)
-                if num is not None:
-                    file_path = os.path.join(root, dir, 'train_results.txt')
-                    spearman_value = get_eval_stsb_spearman(file_path)
-                    if spearman_value is not None:
-                        if num not in this_seed_data:
-                            this_seed_data[num] = []
-                        this_seed_data[num].append(spearman_value)
-                        #this_seed_data.append(spearman_value)
-                else:
-                    print(f"Could not extract number from {dir}")
+                # This will iterate over all the directories in the root
+                for dir in dirs:
+                    
+                    num = extract_number(dir)
+                    if num is not None:
+                        file_path = os.path.join(root, dir, 'train_results.txt')
+                        spearman_value = get_eval_stsb_spearman(file_path)
+                        if spearman_value is not None:
+                            if (seed, num) not in data:
+                                data[(seed, num)] = []
+                            data[(seed, num)].append(spearman_value)
+                            #this_seed_data.append(spearman_value)
+                    else:
+                        print(f"Could not extract number from {dir}")
+            else:
+                print(f"Could not extract seed from {root}")
 
-            for num, this_data in this_seed_data.items():
-                print(len(this_data))
-                if num not in data:
-                    data[num] = []
-                data[num].append(np.array(this_data).max())
+    return_data = {}
+    for key, this_data in data.items():
+        print(len(this_data))
+        seed, num = key
+        if num not in data:
+            return_data[num] = []
+        return_data[num].append(np.array(this_data).max())
+        
+    data = return_data
 
     return data
 
