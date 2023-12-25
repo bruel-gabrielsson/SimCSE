3e59a080799c8ab7da946807c0757b93abaaece4
Rickard Brüel Gabrielsson <rbg@Rickards-MacBook-Pro.local>
2023-12-25 11:48:39 -0500

    M

--- scripts/PCA/run_uni_hyper_REG_debug.sh
+++ scripts/PCA/run_uni_hyper_REG_debug.sh
@@ -40,7 +40,7 @@ do
             size="-1"
             
             #output_dir="/mnt2/brg/simcse-data/HYPER/PCA${size}_REG/PCAREG_L${layer}_b${batch_size}_lr${learning_rate}_s${size}"
-            output_dir="/skunk-pod-storage-brg-40mit-2eedu-pvc/DATA/simcse-data/HYPER/PCA${size}_REG/PCAREG_L${layer}_b${batch_size}_lr${learning_rate}_s${size}"
+            output_dir="/skunk-pod-storage-brg-40mit-2eedu-pvc/DATA/simcse-data/HYPER/PCADEBUG${size}_REG/PCAREG_L${layer}_b${batch_size}_lr${learning_rate}_s${size}"
             echo "device ${device} batch_size ${batch_size} output_dir ${output_dir}"
             CUDA_VISIBLE_DEVICES="${device}" python train.py \
                 --transform_layer $layer \
