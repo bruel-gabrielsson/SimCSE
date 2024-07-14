f1030b555f4d4112359cb60846cef511a49614d1
Rickard Brüel Gabrielsson <rbg@Rickards-MacBook-Pro.local>
2024-07-14 15:00:57 -0400

    M

--- scripts/_run_save.sh
+++ scripts/_run_save.sh
@@ -54,7 +54,7 @@ do
                 --attention_probs_dropout_prob $dropout_rate \
                 --hidden_dropout_prob $dropout_rate \
                 --train_file data/wiki1m_for_simcse.txt \
-                --model_type bert-base-uncased \
+                --model_type bert \
                 --output_dir $output_dir \
                 --num_train_epochs 0 \
                 --per_device_train_batch_size $batch_size \
