e01e930b15fb27204052df79afe003b278066275
Rickard Brüel Gabrielsson <rbg@Rickards-MacBook-Pro.local>
2024-07-15 09:09:44 -0400

    M

--- scripts/_run_save.sh
+++ scripts/_run_save.sh
@@ -43,7 +43,7 @@ do
             #output_dir="/mnt2/brg/simcse-data/HYPER/REG_MLM/REGMLM_L${layer}_b${batch_size}_lr${learning_rate}"
             #output_dir="/skunk-pod-storage-brg-40mit-2eedu-pvc/DATA/simcse-data/HYPER/REG_MLMO_ODA/REGMLMO_L${layer}_b${batch_size}_lr${learning_rate}"
             #output_dir="/skunk-pod-storage-brg-40mit-2eedu-pvc/DATA/simcse-data/HYPER/SUPER_REG_NOV23_S${seed}/SUPREG_${layer}_b${batch_size}_lr${learning_rate}_s${seed}"
-            output_dir="/mnt/brg/simcse-data/HYPER/JUST_SAVE_JULY14_S${seed}/JUST_SAVE_L${layer}_dr${dropout_rate}_b${batch_size}_lr${learning_rate}_s${seed}"
+            output_dir="/mnt/brg/simcse-data/HYPER/JUST_SAVE_JULY14TTT_S${seed}/JUST_SAVE_L${layer}_dr${dropout_rate}_b${batch_size}_lr${learning_rate}_s${seed}"
             echo "device ${device} batch_size ${batch_size} output_dir ${output_dir}"
             # 
             # --transform_layer $layer \
